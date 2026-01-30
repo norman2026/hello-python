fa3cc75fbd5cbd5ead3f77a8518f52e47df9e9fb
Norman Smith <norman_a_smith@hotmail.com>
2026-01-30 19:27:25 +0000

    Day 1 – Python basics: variables and printing

---
+++ day01_basics.py
@@ -0,0 +1,14 @@
+
+#Day1 python basics
+
+name = "Norman Smith"
+role = "Returning to IT"
+yrs_experience=10
+learning_python=True
+
+print (f"Hello World")
+print (f"My name is: {name}")
+print (f"My role is {role}")
+print (f"And I have {yrs_experience} working in IT")
+print (f"Learning Python {learning_python}")
+print (f"A DEMO FOR JAHVAIR")
